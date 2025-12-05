38bc65d4a7507a1f19ab3b2e8b638c5a0ad296cd
Santhoshi-18 <saisanthoshithummepalli06@gmail.com>
2025-12-05 16:27:17 +0530

    Delete PerformanceTracker-main directory

--- PerformanceTracker-main/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import HomePage from "./HomePage";
-import LoginPage from "./LoginPage";
-import SignupPage from "./SignupPage";
-import AcitivityLogsPage from "./AcitivityLogsPage";
-
-
-export { HomePage, LoginPage, SignupPage,AcitivityLogsPage };
